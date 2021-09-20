ebade97b163a065b30261493a34a29dacdffcecb
hcn <1065370468@qq.com>
2021-09-20 12:16:21 +0800

    Use figlet

---
+++ day01/demo-cli/lib/init.js
@@ -0,0 +1,11 @@
+const {promisify} = require('util')
+const figlet = promisify(require('figlet'))
+const clear = require('clear')
+const chalk = require('chalk')
+const log = content => console.log(chalk.green(content))
+
+module.exports = async name =>{
+    clear()
+    const data = await figlet('KKB Welcome')
+    log(data)
+}
