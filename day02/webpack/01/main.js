deb5369b2dd12752573d7064cefefb8275c52ebd
hcn <1065370468@qq.com>
2021-09-21 14:10:59 +0800

    webpack

--- day02/webpack/01/main.js
+++ day02/webpack/01/main.js
@@ -1,5 +1,24 @@
 import { foo } from "./foo"
+import "./style.css"
 
+const div =document.createElement("div");
+
+div.classList.add("red");
+
+div.innerText = "color";
+
+document.body.append(div);
+
+const state = {
+    v:"1",
+};
+
+state.v = 2;
+
+console.log(state);
+
+//非js文件不理解，直接报错，需要把他转换成js
+//用loader资源转换器来转换
 
 
 
--- day02/webpack/01/webpack.config.js
+++ day02/webpack/01/webpack.config.js
@@ -1,7 +1,45 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
+    mode:"development",
     entry:"./main.js",
     output :{
-        path: path.resolve(__dirname, "dist")
-    }
-}
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean:true,
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+        })
+    ],
+    devServer:{
+        static:"./dist",
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                //.css -> .js
+                //从后往前执行
+                use:["style-loader","css-loader"],
+            },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type:"asset/resource",
+            },
+            {
+                test:/\.json5$/i,
+                type:"json",
+                parser :{
+                    parse :json5.parse,
+                }
+            }
+        ],
+    },
+};
+
+// css-loader
+//webpack -> require("css-loader") -> (node_modules)
