10a7c2b7bd6e40e81abd1e7b9f0ad1ab7b7e8b1b
hcn <1065370468@qq.com>
2021-09-22 15:42:55 +0800

    feat: some changes in vue-app and react-app

---
+++ day03/react-app/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const arry = [1,2,3,4,5];
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+        arry.map((element) =>{
+          return(
+          <li key = { element } >
+             <a className = "" > { element} </a>
+          </li >)
+        })
+        }
+        {props.template}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
